9d5a572831f461a58562de4940bde2eeb86055c5
MarcelHerman <herman-marcel@wp.pl>
2024-06-15 21:40:20 +0200

    Tomaszo Migration

---
+++ ProjektSklep/Migrations/202406151939442_TomaszoZmiany.cs
@@ -0,0 +1,22 @@
+﻿namespace ProjektSklep.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TomaszoZmiany : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "lastSpin", c => c.DateTime());
+            AddColumn("dbo.Users", "currDiscount", c => c.String());
+            AlterColumn("dbo.Orders", "discount", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Orders", "discount", c => c.Int());
+            DropColumn("dbo.Users", "currDiscount");
+            DropColumn("dbo.Users", "lastSpin");
+        }
+    }
+}
